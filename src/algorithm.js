4479225dca4968f486504fbe094402f1fa7f2d3d
何仕敬 <hesj1139@13322ty.com>
2018-06-14 18:39:41 +0800

    插入排序添加

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,4 +1,4 @@
-function maopao (arr) {
+module.exports.maopao = function (arr) {
   var _len = arr.length
   for (var i = 0; i < _len; i++) {
     for (var j = i + 1; j < _len; j++) {
@@ -12,7 +12,7 @@ function maopao (arr) {
   return arr
 }
 
-function quick (arr) {
+var quick = function (arr) {
   var length = arr.length
   if (length <= 1) return arr
   var left = [] // 根据正序 反序处理小于基准的数的位置 此处用正序 所以小于基准的放在left
@@ -25,8 +25,30 @@ function quick (arr) {
   }
   return [].concat(quick(left), [pointValue], quick(right))
 }
-
-module.exports = {
-  maopao,
-  quick
+module.exports.quick = quick
+/**
+ * 思路描述
+ * 首先从第二个开始处理，默认第一个是有序的
+ * 记录当前开始位置@param{temp}，如第@param{i=4}个 则开始比较的第一个数为第@param{j=i-1}个
+ * 进入循环，如果@param{j}>=0 保证 j--之后最小为-1,且小于（根据正序 还是 反序切换）temp
+ * 交换比较的两个数的位置，因为temp一直在缓存中，只需要当前j+1位置更新为j位置的数，例子如下
+ * [1,3,4,5,2] 假设当前位置为4 => temp = 2, i = 4, j = 3
+ * j >= 0 && 2 < 5 => [1, 3, 4, 5, 5] j-- => j = 2
+ * j >= 0 && 2 < 4 => [1, 3, 4, 4, 5] j-- => j = 1
+ * j >= 0 && 2 < 3 => [1, 3, 3, 4, 5] j-- => j = 0
+ * j >= 0 && 2 不小于 1 退出循环 j = 0
+ * 数组j+1 的位置 替换为 temp 完成一次插入
+ */
+module.exports.pushSort = function (arr) {
+  var len = arr.length, tmp, i, j
+  for (i = 1; i < len; i++) {
+    tmp = arr[i] // 缓存当前
+    j = i - 1 // 记录开始比较位置，以当前索引的后一个
+    while (j>=0 && tmp < arr[j]) {
+      arr[j+1] = arr[j]
+      j--
+    }
+    arr[j+1] = tmp
+  }
+  return arr
 }
--- test/algorithm.test.js
+++ test/algorithm.test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var algorithm = require('../src/algorithm')
+console.log(algorithm)
 var utils = require('../src/utils')
 describe('algorithm', () => {
   // mocha -g maopao
@@ -13,10 +14,20 @@ describe('algorithm', () => {
   // 快速排序
   it('quick', () => {
     var quick = algorithm.quick
-    var GarbleArray = utils.GarbleArray
-    for (var i = 0; i < 10; i++) {
+    // 测试1000次
+    for (var i = 0; i < 1000; i++) {
       var testArr = utils.RandomArray(30, 100)
       var result = quick(Object.assign([], testArr))
+      expect(JSON.stringify(result)).to.equal(JSON.stringify(testArr.sort((a, b) => a - b)))
+    }
+  })
+  // 插入排序
+  it('pushSort', () => {
+    var pushSort = algorithm.pushSort
+    // 测试1000次
+    for (var i = 0; i < 1000; i++) {
+      var testArr = utils.RandomArray(20, 100)
+      var result = pushSort(Object.assign([], testArr))
       expect(JSON.stringify(result)).to.equal(JSON.stringify(testArr.sort((a, b) => a - b)))
     }
   })
