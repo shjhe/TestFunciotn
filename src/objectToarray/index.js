d7bf107bc22eaefa60b73218fdd8948311fbc090
何仕敬 <hesj1139@13322ty.com>
2018-07-13 10:59:12 +0800

    优化算法

--- src/objectToarray/index.js
+++ src/objectToarray/index.js
@@ -1,74 +1,25 @@
-// var obj = {
-//   a: {
-//     b: {
-//       c: {
-//         d: 'h',
-//         j: 'l',
-//         o: {
-//           p: 'q',
-//           r: 's'
-//         },
-//         t: 'u'
-//       }
-//     },
-//     v: {
-//       w: {
-//         x: {
-//           y: 'z'
-//         }
-//       }
-//     }
-//   },
-//   e: {
-//     f: {
-//       i: 'k'
-//     },
-//     m: 'n'
-//   }
-// }
-
-// //输出结果，按照层数，把同一层的属性放在同一个子数组内
+// var obj = {"a":{"b":{"c":{"d":"h","j":"l","o":{"p":"q","r":"s"},"t":"u"}},"v":{"w":{"x":{"y":"z"}}}},"e":{"f":{"i":"k"},"m":"n"}}
+//输出结果，按照层数，把同一层的属性放在同一个子数组内
 // var result = [[a, e], [b, v, f, m], [c, w, i], [d, j, o, t, x], [p, r, y]];
-function getProp(obj) {
-  //输出结果数组
-  var result = [];
-  //递归层数，也就是属性层数
-  var num = 0;
 
-  function temp(obj) {
-    //获取对象的属性数组
-    var keys = Object.keys(obj);
-    var len = keys.length;
-    for (var i = 0; i < len; i++) {
-      //判断属性值，如果是对象，则递归遍历
-      if (typeof obj[keys[i]] == 'object') {
-        // 如果属性是对象，层数加一
-        num++;
-        //用result[num-1]存储每一层的结果，如果该层首次存储属性名称，初始化为空数组
-        if (typeof result[num - 1] != 'object') {
-          result[num - 1] = [];
-        }
-        result[num - 1].push(keys[i]);
-        temp(obj[keys[i]]);
-        //注意当一层的所有属性都遍历完之后，返回上一层
-        if (i == len - 1) {
-          num--;
-        }
-      } else {
-        //如果属性不是层数，那么先对层数加一，存入后再减一
-        num++;
-        if (typeof result[num - 1] != 'object') {
-          result[num - 1] = [];
-        }
-        result[num - 1].push(keys[i]);
-        num--;
-        if (i == len - 1) {
-          num--;
-        }
-      }
+// 解决思路
+/*
+ *  给每层对象下添加一个索引属性，初始下为0，逐层加1
+ *  初始化一个数组，每个索引下新建一个数组，并保持添加当前递归层的key
+ */
+
+function getProp(obj, result) {
+  result = result || []
+  obj._index = obj._index || 0
+  result[obj._index] = result[obj._index] || []
+  for(var key in obj) {
+    if (key === '_index') continue
+    result[obj._index].push(key)
+    if (typeof obj[key] === 'object') {
+      obj[key]._index = obj._index + 1
+      getProp(obj[key], result)
     }
   }
-  temp(obj)
   return result
 }
 
