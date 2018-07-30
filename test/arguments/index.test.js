6fb67c77f1a2b4847b0f06a372bd221fd217ab58
何仕敬 <hesj1139@13322ty.com>
2018-07-30 18:03:28 +0800

    优化【arguments】测试

--- test/arguments/index.test.js
+++ test/arguments/index.test.js
@@ -2,8 +2,8 @@ var expect = require('chai').expect;
 var arguments = require('../../src/arguments')
 var utils = require('../../src/utils')
 describe('arguments', () => {
-  // 测试文件不是在test文件夹下 需要加--recursive
-  // mocha -g --recursive sumArr
+  // 测试文件不是在test文件夹下 需要加--recursive 其后添加文件路径
+  // mocha -g --recursive "./test/arguments/*.js"
   it('sumArr', () => {
     var sumArr = arguments.sumArr
     var result = sumArr(1)(2,3,4,5).valueOf()
