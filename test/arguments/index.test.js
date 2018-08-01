353835ca2aa02e5789cab69c1b228605eff25332
shjhe <568675732@qq.com>
2018-08-01 23:44:41 +0800

    修改测试命令

--- test/arguments/index.test.js
+++ test/arguments/index.test.js
@@ -3,13 +3,14 @@ var arguments = require('../../src/arguments')
 var utils = require('../../src/utils')
 describe('arguments', () => {
   // 测试文件不是在test文件夹下 需要加--recursive 其后添加文件路径
-  // mocha -g --recursive "./test/arguments/*.js"
+  // mocha --recursive "./test/arguments/*.js"
   it('sumArr', () => {
     var sumArr = arguments.sumArr
     var result = sumArr(1)(2,3,4,5).valueOf()
     // 测试是否能清空 保留数据
     var result2 = sumArr(1)(2,3,4,5).valueOf()
-    console.log(result, result2)
+    var result3 = sumArr(1)(2,3,4,5)(10,9).valueOf()
+    console.log(result, result2, result3)
     expect(result).equals(15)
   })
 })
